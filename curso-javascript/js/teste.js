62d8af6fcce5bf29a2e0090bd58b4061c36f9e35
Alex Ferreira <asfuture2019@gmail.com>
2022-06-24 22:36:45 -0300

    Criando um contador com javascript

---
+++ curso-javascript/js/teste.js
@@ -0,0 +1,20 @@
+//Bootcamp Impulso JavaScript Evolution
+/*
+Autor:Alex Ferreira
+
+var currrentNumberWrapper = document.getElementById("currentNumber");
+var currrentNumber = 0;
+
+function increment(){
+    currrentNumber = currrentNumber + 1;
+    currrentNumberWrapper.innerHTML = currrentNumber;
+}
+function decrement(){
+    currrentNumber = currrentNumber - 1;
+    currrentNumberWrapper.innerHTML = currrentNumber;
+}
+*/
+function soma(a,b){
+    return a+b;
+}
+soma();
