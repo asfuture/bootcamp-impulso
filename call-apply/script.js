5c7bfed7e553a384f9b9041bc286eeeeb488e2a5
Alex Ferreira <asfuture2019@gmail.com>
2022-06-28 18:45:52 -0300

    aula-apply

---
+++ call-apply/script.js
@@ -0,0 +1,56 @@
+// const alunos = [
+//     {
+//         nome:'Alex',
+//         nota:5,
+//         turma:'18'
+//     },
+//     {
+//         nome:'Santos',
+//         nota:9,
+//         turma:'18'
+//     },
+//     {
+//         nome:'Ferreira',
+//         nota:6,
+//         turma:'2C'
+//     },
+// ];
+
+// //Solução 1° atividade
+// function alunoAprovados(arr,media){
+//     let aprovados = [];
+//     for(let i=0; i<arr.length;i++){
+//         const {nota,nome} = arr[i];
+//         if(nota >= media){
+//             aprovados.push(nome)
+//         }
+//     }
+//     return aprovados;
+// }
+
+// console.log(alunoAprovados(alunos, 5));
+
+// //Solução para 2°  ativida
+// function calculaIdade(anos){
+// return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
+// }
+
+// const peasso1 = {
+//     nome:"Alene",
+//     idade:26
+// }
+// const peasso2 = {
+//     nome:"Alex",
+//     idade:30
+// }
+// const animal = {
+//     nome:"Elaine",
+//     idade:5,
+//     race:"Pug"
+// }
+
+// console.log(calculaIdade.apply(peasso1, [2]));
+
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i])
+  }
