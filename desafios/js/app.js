f132061a79f085d9999b26c711ef6317c9775f82
Alex Ferreira <asfuture2019@gmail.com>
2022-07-03 12:41:29 -0300

    Resolução dos desafios 1,2,3

--- desafios/js/app.js
+++ desafios/js/app.js
@@ -1,4 +1,6 @@
 "use strict";
+//########################################################################
+// Atividade 0 calculando dois numeros 
 let button = document.getElementById("button");
 let num1 = document.getElementById('num1');
 let num2 = document.getElementById('num2');
@@ -18,12 +20,23 @@ if (button) {
         }
     });
 }
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-const matricula = {
-    nome: 'Alex Ferreira',
-    code: 10
-};
-// Como podemos melhorar o esse código usando TS? 
+//########################################################################
+//ATIVIDADE 1 Como podemos rodar isso em um arquivo .ts sem causar erros? 
+let btndesafio = document.getElementById('btndesafio1');
+function desafio() {
+    const matricula = {
+        nome: 'Alex Ferreira',
+        code: 10
+    };
+    alert(matricula.nome + ", " + matricula.code);
+}
+if (btndesafio) {
+    btndesafio.addEventListener('click', () => {
+        desafio();
+    });
+}
+//########################################################################
+// ATIVIDADE 2 Como podemos melhorar o esse código usando TS? 
 const pessoa = (nome, idade, profissao) => {
     nome = 'Paris',
         idade = 26,
@@ -61,11 +74,18 @@ const pessoa5 = {
     idade: 33,
     profissao: profissao.Desenvolvedor
 };
+let respotaDesafio2 = document.getElementById('respotaDesafio2');
 function listar() {
     console.log(pessoa1, pessoa2, pessoa3, pessoa4, pessoa5);
+    alert(pessoa1.nome + " " + pessoa2.nome + " " + pessoa3.nome + " " + pessoa4.nome + " " + pessoa5.nome);
 }
-listar();
-// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+if (respotaDesafio2) {
+    respotaDesafio2.addEventListener('click', () => {
+        listar();
+    });
+}
+//##########################################################################################
+//ATIVIDADE 3 O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
 let botaoAtualizar = document.getElementById('atualizar-saldo');
 let botaoLimpar = document.getElementById('limpar-saldo');
 let soma = document.getElementById('soma');
